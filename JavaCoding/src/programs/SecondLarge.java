bbc0073c914434994c9b47fd3181ca6123ea6f16
AishwaryalakshmiD <aishwaryalakshmi27122000@gmail.com>
2026-04-02 19:56:29 +0530

    Second Largest in Array

---
+++ JavaCoding/src/programs/SecondLarge.java
@@ -0,0 +1,21 @@
+package programs;
+
+public class SecondLarge {
+	public static void main(String[] args) {
+		int[] arr = {10, 45, 23, 89, 12,87};
+		
+		for(int i = 0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]>arr[j]) {
+					int temp = arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
+		}
+		
+		System.out.println(arr[arr.length-2]);
+
+	}
+
+}
